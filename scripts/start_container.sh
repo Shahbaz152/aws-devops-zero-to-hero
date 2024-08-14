22c59048894c155296cdb737e0fa2fa0070a3861
Shahbaz152 <65075028+Shahbaz152@users.noreply.github.com>
2024-08-15 00:32:05 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
+docker pull shahbaz152/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 6000:6000 abhishekf5/simple-python-flask-app
+docker run -d -p 6000:6000 shahbaz152/simple-python-flask-app:latest
